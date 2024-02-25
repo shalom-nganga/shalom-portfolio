beb84bb6e20cb9117ee9a0ca3ccdc52ce8de56f3
shalom-nganga <minnomacharia@gmail.com>
2024-02-25 19:12:59 +0300

    85% done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let newIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classlist.toggle( 'fa-xmark');
-    navbar.classList.toggle('active')
-}
+newIcon.onclick = () => {
+    newIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
 
 
 let sections = document.querySelectorAll('section');
@@ -12,15 +12,15 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections .forEach(sec => {
-            let top = widow.scrollY;
+            let top = window.scrollY;
             let offset = sec.offsetTop - 150;
             let height = sec.offsetHeight;
             let id = sec.getAttribute('id');
 
             if(top >= offset && top < offset + height) {
-                navLinks.forEach.apply(links => {
+                navLinks.forEach(links => {
                     links.classList.remove('active');
-                    document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
                 });
             };
         });
@@ -30,7 +30,33 @@ window.onscroll = () => {
         header.classList.toggle('sticky', window.scrollY > 100);
 
 
-        menuIcon.classList.remove('fa-xmark');
+        newIcon.classList.remove('fa-xmark');
         navbar.classList.remove('active');
 
 };
+
+
+        /*######################## scroll reveal ############################*/
+
+ ScrollReveal({ 
+     distance: '80px',
+     duration:2000,
+     delay:200,
+});
+
+ ScrollReveal().reveal('.home-content, heading',{origin:'top'});
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin:'button'});
+ ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
+ ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
+
+ /*######################## Typed js ############################*/
+
+ var typed = new Typed('.multiple-text', {
+    strings: ["Fullstack Developer.", "Web Designer.", "App Designer."],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 900,
+    loop: true,
+  });
+
+        
